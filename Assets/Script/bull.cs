3092df6a4f5e5cd621a9184bff37d70c664fcb0f
Nartys <c.natrys@gmail.com>
2021-01-18 11:12:10 +0100

    ajout d'une ia qui charge

--- Assets/Script/bull.cs
+++ Assets/Script/bull.cs
@@ -15,8 +15,11 @@ public class bull : MonoBehaviour
     public float Range = 0f;
     private bool OutRange = true;
 
-    private bool Charge = false;
+
     private float charging = 0f;
+
+    public Vector2 worldLimits;
+
     //public float RadiusOfRaycast;
 
     // Start is called before the first frame update
@@ -28,45 +31,45 @@ public class bull : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 position = transform.position;
+
+        
+        position.x = Mathf.Clamp(transform.position.x, -worldLimits.x, worldLimits.x);
+        position.y = Mathf.Clamp(transform.position.y, -worldLimits.y, worldLimits.y);
+        transform.position = position;
 
         Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.right) * Radius, Color.green);
 
         //RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.right), Range);
         RaycastHit2D hit = Physics2D.CircleCast(transform.position, Radius, transform.TransformDirection(Vector2.right), Range);
-        hit.transform.GetComponent<SpriteRenderer>().color = Color.green;
+
 
         if (hit)
         {
             charging += Time.deltaTime;
-        }
-        else
-        {
-            OutRange = true;
-        }
-
-       /* if (OutRange == true)
-        {
-            Patrolling();
-        }*/
-
-        if (charging > 2)
-        {
-            charging = 0;
-            Charge = true;
-        }
-        if (Charge == true)
-        {
-
+            hit.transform.GetComponent<SpriteRenderer>().color = Color.red;
 
             Vector3 direction = Player.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             rb.rotation = angle;
-            direction.Normalize();
-            movement = direction;
-
-            OutRange = false;
-            moveSpeed = 50f;
         }
+        else
+        {
+            OutRange = true;
+            charging = 0;
+        }
+
+        if (OutRange == true)
+        {
+            Patrolling();
+        }
+
+        if (charging > 2)
+        {
+            charging = 0;
+            ChargeBull();
+        }
+ 
     }
     private void FixedUpdate()
     {
@@ -81,5 +84,29 @@ public class bull : MonoBehaviour
     {
         moveSpeed = 0f;
     }
+
+    public void ChargeBull()
+    {
+        Vector3 direction = Player.position - transform.position;
+       // float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+       // rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+
+        OutRange = false;
+        moveSpeed = 50f;
+        Radius = 20f;
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, Radius);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(Vector3.zero, new Vector3(worldLimits.x * 2, worldLimits.y * 2, 1.0f));
+    }
+
 }
 
