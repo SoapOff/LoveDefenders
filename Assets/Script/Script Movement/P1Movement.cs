8b79a64c05c5ee24c52325446819cab6b6b55e62
Mathéo Million <matheomillion@gmail.com>
2021-01-28 14:33:00 +0100

    add animation

--- Assets/Script/Script Movement/P1Movement.cs
+++ Assets/Script/Script Movement/P1Movement.cs
@@ -7,6 +7,7 @@ public class P1Movement : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveDirection;
     [SerializeField] private float moveSpeed = 60f;
+    private bool facingRight = true;
 
     void Start()
     {
@@ -20,11 +21,26 @@ public class P1Movement : MonoBehaviour
 
     void InputMovement()
     {
+        Vector3 localScale = transform.localScale;
         float moveX = Input.GetAxisRaw("Vertical");
         float moveY = Input.GetAxisRaw("Horizontal");
 
         moveDirection = new Vector2(moveY, moveX).normalized;
-        //rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
         rb.AddForce(new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed));
+
+        //rotate sprite
+        if (moveY > 0)
+        {
+            facingRight = true;
+        }
+        else if (moveY < 0)
+        {
+            facingRight = false;
+        }
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+        {
+            localScale.x *= -1;
+        }
+        transform.localScale = localScale;
     }
 }
