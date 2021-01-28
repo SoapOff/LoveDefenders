ea226ec39514a0ebe37fd1a80729007816f933be
Nartys <c.natrys@gmail.com>
2021-01-28 15:25:07 +0100

    ajout de la tourelle et du rebond

--- Assets/Script/Script IA/IABull.cs
+++ Assets/Script/Script IA/IABull.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IATest : MonoBehaviour
+public class IABull : MonoBehaviour
 {
 
     public Transform Player;
@@ -15,7 +15,8 @@ public class IATest : MonoBehaviour
     public float Range = 25f;
     private bool OutRange = true;
 
-
+    private float Charging = 0f;
+    private bool IsCharging = false;
     //public float RadiusOfRaycast;
 
     // Start is called before the first frame update
@@ -28,32 +29,55 @@ public class IATest : MonoBehaviour
     void Update()
     {
 
-     Debug.DrawRay(transform.position,transform.TransformDirection(Vector2.right) * 25f, Color.green);
-     
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.right) * 25f, Color.green);
+
         //RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.right), Range);
-        RaycastHit2D hit = Physics2D.CircleCast(transform.position, Radius, transform.TransformDirection(Vector2.right),Range);
+        RaycastHit2D hit = Physics2D.CircleCast(transform.position, Radius, transform.TransformDirection(Vector2.right), Range);
 
         if (hit)
         {
-            hit.transform.GetComponent<SpriteRenderer>().color = Color.green;
-
+            Charging += Time.deltaTime;
+            Debug.Log(Charging);
             Vector3 direction = Player.position - transform.position;
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             rb.rotation = angle;
-            direction.Normalize();
-            movement = direction;
+           // direction.Normalize();
 
             OutRange = false;
-            moveSpeed = 12f;
+           
         }
         else
-        {   
+        {
             OutRange = true;
+            IsCharging = false;
         }
 
         if (OutRange == true)
         {
             Patrolling();
+        }
+
+        if (Charging > 2.2f)
+        {
+            IsCharging = true;
+
+            moveSpeed = 12f;
+
+        }
+      if (IsCharging == true)
+        {
+            Vector3 direction = Player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            //rb.rotation = angle;
+            direction.Normalize();
+            movement = direction;
+            
+            if (OutRange == true)
+            { 
+            Charging = 0;
+            IsCharging = false;
+           // moveSpeed = 0f;
+            }
         }
     }
     private void FixedUpdate()
@@ -68,11 +92,5 @@ public class IATest : MonoBehaviour
     public void Patrolling()
     {
         moveSpeed = 0f;
-    }
-    void OnDrawGizmos()
-    {
-        // Draw a yellow sphere at the transform's position
-        Gizmos.color = Color.green;
-        Gizmos.DrawSphere(transform.position, Radius);
     }
 }
